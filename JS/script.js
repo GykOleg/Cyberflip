ee4c46d8931e3e8e77cefc4c487a53e0145dbc0c
GykOleg <gygyryk@gmail.com>
2022-07-14 17:35:01 +0300

    Tablet half adaptive

--- JS/script.js
+++ JS/script.js
@@ -2,4 +2,7 @@ $(function () {
    $(".header__menu").click(function () {
       $(".mobile-menu__body").toggleClass("menu-open");
    });
+   $(".footer__title-small").click(function () {
+      $(this).toggleClass("active").next().slideToggle(500);
+   });
 });
