d0ba44322112d8ed12ae0b392e78313e019c6b22
GykOleg <gygyryk@gmail.com>
2022-07-15 15:55:54 +0300

    Correctives and refactoeing

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 $(function () {
+   $(".mobile-menu__body").hide();
    $(".header__menu").click(function () {
-      $(".mobile-menu__body").toggleClass("menu-open");
+      $(".mobile-menu__body").slideToggle(500);
    });
    $(".footer__title-small").click(function () {
       $(this).toggleClass("active").next().slideToggle(500);
