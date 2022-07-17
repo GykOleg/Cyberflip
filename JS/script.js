3269de8f0b94208f3010c2762b5e15539f466c1c
GykOleg <gygyryk@gmail.com>
2022-07-17 20:00:05 +0300

    Slick slider connect

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ $(function () {
    $(".header__menu").click(function () {
       $(".mobile-menu__body").slideToggle(500);
    });
+   $(".slider").slick();
    $(".footer__title-small").click(function () {
       $(this).toggleClass("active").next().slideToggle(500);
    });
