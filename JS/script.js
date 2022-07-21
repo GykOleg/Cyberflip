e50374c66f8d17e985b63841fd0d7054e7929aea
GykOleg <gygyryk@gmail.com>
2022-07-21 15:31:49 +0300

    New pages html code

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,29 @@
 $(function () {
-   setTimeout(function () {
+   //loading screen
+   /*setTimeout(function () {
       $(".loading__middle").slideUp(300);
       setTimeout(function () {
          $(".loading").fadeToggle(700);
          $(".body").removeClass("scroll-lock");
       }, 500);
-   }, 10000);
+   }, 10000);*/
+   $(".loading").fadeToggle(700);
+   $(".body").removeClass("scroll-lock");
+   //burger menu
    $(".mobile-menu__body").hide();
    $(".header__menu").click(function () {
       $(".mobile-menu__body").slideToggle(500);
    });
+   //slider
    $(".slider").slick({
       dots: true
    });
+   //adaptive spoiler
    if (window.matchMedia('all and (max-width: 900px').matches) {
       $(".footer__title-small").click(function () {
          $(this).toggleClass("active").next().slideToggle(500);
       });
    }
+   //stiky services
+
 });
